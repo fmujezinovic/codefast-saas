a91483f01707d1592533791919d3005b04e48357
fmujezinovic <53880402+fmujezinovic@users.noreply.github.com>
2025-01-08 12:21:21 +0100

    Add hero and header section

---
+++ components/ButtonLogin.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const ButtonLogin = ({ isLoggedIn, name}) => {
+
+    if (isLoggedIn) {
+        return <Link href="/dashboard" className="btn btn-primary">Go to dashboard</Link>
+    } else {
+        return <button>Login</button>;
+    }
+
+};
+
+export default ButtonLogin;
