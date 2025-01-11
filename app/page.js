81da91bb60f83e4cdf47f36fd2f4b8fb429c6b93
fmujezinovic <53880402+fmujezinovic@users.noreply.github.com>
2025-01-11 09:59:46 +0100

    polish homepage

--- app/page.js
+++ app/page.js
@@ -1,24 +1,38 @@
 import ButtonLogin from "@/components/ButtonLogin";
+import ListItem from "@/components/ListItems";
+import FAQListItem from "@/components/FAQListItem";
 
 export default function Home() {
   const isLoggedIn = true;
   const name = "Marc";
+  const pricingFeaturesList = [
+    "Collect customer feedback",
+    "Unlimited boards",
+    "Admin dashboard",
+    "24h/7 support",
+  ];
 
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-base-200">
         <div className="max-w-3xl mx-auto  flex justify-between items-center px-8 py-2">
           <div>CodeFastSaas</div>
           <div className="space-x-4 max-md:hidden">
-            <a className="link link-hover">Pricing</a>
-            <a className="link link-hover">FAQ</a>
+            <a className="link link-hover" href="#pricing">
+              Pricing
+            </a>
+            <a className="link link-hover" href="#faq">
+              FAQ
+            </a>
           </div>
           <div>
             <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
           </div>
         </div>
       </section>
-      <section className="text-center py-32 px-8 max-w-3xl mx-auto">
+      {/* HERO ne rabis bg-base-100*/}
+      <section className="bg-base-100 text-center py-32 px-8 max-w-3xl mx-auto">
         <h1 className="text-4xl font-extrabold mb-6">
           Collect customer feedback to create better products
         </h1>
@@ -28,6 +42,80 @@ export default function Home() {
         </div>
 
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+      {/* PRICING */}
+      <section className="bg-base-200" id="pricing">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            Pricing that adapts to your needs
+          </h2>
+          <div className="p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-6">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">19$</div>
+              <div className="text-sm uppercase font-medium opacity-60">
+                /month
+              </div>
+            </div>
+            <ul className="space-y-2">
+              {pricingFeaturesList.map((priceItem) => (
+                <li className="flex gap-2 items-center" key={priceItem}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="text-green-600 size-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 12.75 6 6 9-13.5"
+                    />
+                  </svg>
+                  {priceItem}
+                </li>
+              ))}
+            </ul>
+            <ButtonLogin
+              isLoggedIn={isLoggedIn}
+              name={name}
+              extraStyle="w-full"
+            />
+          </div>
+        </div>
+      </section>
+      {/* FAQ */}
+      <section className="bg-base-100" id="faq">
+        <div className="py-32 px-8 max-w-3xl mx-auto text-center">
+          <p className="text-sm text-primary uppercase font-medium text-center">
+            FAQ
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12">
+            Frequently Asked Questions
+          </h2>
+          <ul className="max-w-lg">
+            {[
+              {
+                question: "What do I get exactly",
+                answer: "Loreum Ipseum",
+              },
+              {
+                question: "Can I get a refund?",
+                answer: "Loreum Ipseum",
+              },
+              {
+                question: "I have another question!",
+                answer: "Loreum Ipseum",
+              },
+            ].map((qa) => (
+              <FAQListItem key={qa.question} qa={qa} />
+            ))}
+          </ul>
+        </div>
       </section>
     </main>
   );
--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,13 +1,18 @@
 import Link from "next/link";
 
-const ButtonLogin = ({ isLoggedIn, name}) => {
-
-    if (isLoggedIn) {
-        return <Link href="/dashboard" className="btn btn-primary">Go to dashboard</Link>
-    } else {
-        return <button>Login</button>;
-    }
-
+const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
+  if (isLoggedIn) {
+    return (
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      >
+        Go to dashboard
+      </Link>
+    );
+  } else {
+    return <button>Login</button>;
+  }
 };
 
 export default ButtonLogin;
