67443d0230e39ba2e823630d5e1a0e2c70cdea9e
fmujezinovic <53880402+fmujezinovic@users.noreply.github.com>
2025-01-19 12:15:27 +0100

    user-boards-list

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,19 @@
 import ButtonLogout from "@/components/ButtonLogout";
 import FormNewBoard from "@/components/FormNewBoard";
+import { auth } from "@/auth";
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
 
-export default function Dashboard() {
+async function getUser() {
+  const session = await auth();
+
+  await connectMongo();
+  return await User.findById(session.user.id).populate("boards");
+}
+
+export default async function Dashboard() {
+  const user = await getUser();
+
   return (
     <main className="bg-base-200 min-h-screen">
       {/* HEADER */}
@@ -12,8 +24,22 @@ export default function Dashboard() {
       </section>
 
       {/* MAIN CONTENT */}
-      <section className="max-w-5xl mx-auto px-5 py-12">
+      <section className="max-w-5xl mx-auto px-5 py-12 space-y-12">
         <FormNewBoard />
+        <div>
+          <h1 className="font-extrabold text-xl mb-4">
+            {user.boards.length} Boards
+          </h1>
+          <ul className="space-y-4">
+            {user.boards.map((board) => {
+              return (
+                <div key={board._id} className="bg-base-100 p-6 rounded-3xl">
+                  {board.name}
+                </div>
+              );
+            })}
+          </ul>
+        </div>
       </section>
     </main>
   );
